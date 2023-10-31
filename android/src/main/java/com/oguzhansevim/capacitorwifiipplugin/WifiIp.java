078a9b743a3c3534d8a882d0a9505b3ea7d60d55
Oğuzhan Sevim <oguzhansevim@outlook.com>
2023-10-31 18:45:17 +0100

    Android integration done

--- android/src/main/java/com/oguzhansevim/capacitorwifiipplugin/WifiIp.java
+++ android/src/main/java/com/oguzhansevim/capacitorwifiipplugin/WifiIp.java
@@ -1,11 +1,51 @@
 package com.oguzhansevim.capacitorwifiipplugin;
 
-import android.util.Log;
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.net.ConnectivityManager;
+
+import com.getcapacitor.Bridge;
+import com.getcapacitor.JSObject;
+import com.getcapacitor.PluginCall;
+
+
+import java.util.Locale;
 
 public class WifiIp {
 
-    public String echo(String value) {
-        Log.i("Echo", value);
-        return value;
+    WifiManager wifiManager;
+    ConnectivityManager connectivityManager;
+    Context context;
+
+    Bridge bridge;
+
+    public void load(Bridge bridge) {
+        this.bridge = bridge;
+        this.wifiManager = (WifiManager) this.bridge.getActivity().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        this.connectivityManager =
+            (ConnectivityManager) this.bridge.getActivity()
+                .getApplicationContext()
+                .getApplicationContext()
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        this.context = this.bridge.getContext();
+    }
+
+    public void getIP(PluginCall call) {
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        int ip = wifiInfo.getIpAddress();
+        String ipString = formatIP(ip);
+
+        if (ipString != null && !ipString.equals("0.0.0.0")) {
+            JSObject result = new JSObject();
+            result.put("ip", ipString);
+            call.resolve(result);
+        } else {
+            call.reject("NO_VALID_IP_IDENTIFIED");
+        }
+    }
+
+    private String formatIP(int ip) {
+        return String.format(Locale.ENGLISH, "%d.%d.%d.%d", (ip & 0xff), (ip >> 8 & 0xff), (ip >> 16 & 0xff), (ip >> 24 & 0xff));
     }
 }
--- android/src/main/java/com/oguzhansevim/capacitorwifiipplugin/WifiIpPlugin.java
+++ android/src/main/java/com/oguzhansevim/capacitorwifiipplugin/WifiIpPlugin.java
@@ -1,22 +1,50 @@
 package com.oguzhansevim.capacitorwifiipplugin;
 
-import com.getcapacitor.JSObject;
+import android.Manifest;
+import android.os.Build;
+
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
+import com.getcapacitor.PermissionState;
+import com.getcapacitor.annotation.Permission;
 import com.getcapacitor.annotation.CapacitorPlugin;
+import com.getcapacitor.annotation.PermissionCallback;
 
-@CapacitorPlugin(name = "WifiIp")
+@CapacitorPlugin(
+    name = "WifiIp",
+    permissions = { @Permission(alias = "fineLocation", strings = { Manifest.permission.ACCESS_FINE_LOCATION }) }
+)
 public class WifiIpPlugin extends Plugin {
 
-    private WifiIp implementation = new WifiIp();
+    private static final int API_VERSION = Build.VERSION.SDK_INT;
+
+    WifiIp wifiIp;
+
+    @Override
+    public void load() {
+        super.load();
+        this.wifiIp = new WifiIp();
+        this.wifiIp.load(this.bridge);
+    }
 
     @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
+    public void getIP(PluginCall call) {
+        if (API_VERSION >= 23 && getPermissionState("fineLocation") != PermissionState.GRANTED) {
+            requestPermissionForAlias("fineLocation", call, "accessFineLocation");
+        } else {
+            this.wifiIp.getIP(call);
+        }
+    }
 
-        JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
-        call.resolve(ret);
+    @PermissionCallback
+    private void accessFineLocation(PluginCall call) {
+        if (getPermissionState("fineLocation") == PermissionState.GRANTED) {
+            if (call.getMethodName().equals("getIP")) {
+                this.wifiIp.getIP(call);
+            }
+        } else {
+            call.reject("User denied permission");
+        }
     }
 }
